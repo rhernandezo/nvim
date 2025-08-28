72e10b30ee01c3f325f0d58e170cf882f277f64f
rhernandezz <ricardo.hernandez@lirmi.com>
2025-08-28 18:19:41 -0400

    feat(plugins): muestra archivos modificados en dashboard

--- lua/plugins/alpha-nvim.lua
+++ lua/plugins/alpha-nvim.lua
@@ -6,8 +6,6 @@ return {
       local dashboard = require('alpha.themes.dashboard')
 
       dashboard.section.header.val = {
-        -- Aquí podrías poner el bigote en ASCII si lo tienes
-        -- O si es un icono de Nerd Font, lo agregas acá
         '                                         ',
         ' ██╗    ██╗██╗ ██████╗██╗  ██╗ ██████╗   ',
         ' ██║    ██║██║██╔════╝██║  ██║██╔═══██╗  ',
@@ -16,18 +14,54 @@ return {
         ' ╚███╔███╔╝██║╚██████╗██║  ██║╚██████╔╝  ',
         '  ╚══╝╚══╝ ╚═╝ ╚═════╝╚═╝  ╚═╝ ╚═════╝   ',
         '                                         ',
-        '        -- Ricardo Hernández --          ',
       }
-
 
       dashboard.section.buttons.val = {
         dashboard.button('f', ' ' .. ' Find File', ':Telescope find_files <CR>'),
         dashboard.button('n', ' ' .. ' New File', ':ene <bar> startinsert <CR>'),
-        dashboard.button('p', ' ' .. ' Find Project', ':Telescope projects <CR>'),
-        dashboard.button('s', '󰋋 ' .. ' Restore Session', ':lua require("persistence").load() <CR>'),
+        dashboard.button('m', ' ' .. ' Modified Files', ':lua OpenModifiedFiles()<CR>'),
         dashboard.button('l', ' ' .. ' Open Lazy', ':Lazy <CR>'),
         dashboard.button('q', '󰅙 ' .. ' Quit Neovim', ':qa<CR>'),
       }
+
+      -- Función para abrir los archivos modificados en buffer
+      function OpenModifiedFiles()
+        local handle = io.popen('git ls-files -m')
+        if handle then
+          local result = handle:read('*a')
+          handle:close()
+          for file in result:gmatch('[^\r\n]+') do
+            vim.cmd('edit ' .. file)
+          end
+        else
+          print('Git not found or no modified files')
+        end
+      end
+
+      -- Footer
+      -- Fn: Muestra los archivos modificados en el footer
+      -- Requiere que git esté instalado y que el directorio sea un repositorio git
+      local function footer()
+        local handle = io.popen('git ls-files -m')
+        if handle then
+          local result = handle:read('*a')
+          handle:close()
+          local modified_files = {}
+          for file in result:gmatch('[^\r\n]+') do
+            table.insert(modified_files, file)
+          end
+          if #modified_files > 0 then
+            -- return 'Modified files: ' .. table.concat(modified_files, ', ')
+            return 'Modified files:\n' .. table.concat(modified_files, '\n')
+          else
+            return 'No modified files'
+          end
+        else
+          return 'Git not found or no modified files'
+        end
+      end
+
+      dashboard.section.footer.val = footer()
 
       alpha.setup(dashboard.config)
     end
